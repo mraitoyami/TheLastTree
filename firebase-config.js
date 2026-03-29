6dafed44be271a47794df66a8cc5b6078d3bbbce
mraitoyami <mraitoyami@gmail.com>
2026-03-29 16:01:48 +0800

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-window.FIREBASE_LEADERBOARD_CONFIG = {
-  apiKey: "AIzaSyCMv05E-Q_Wqsx2rEQ4DuFWFJ9pOmEsmQM",
-  authDomain: "trashbin2.firebaseapp.com",
-  projectId: "trashbin2",
-  storageBucket: "trashbin2.firebasestorage.app",
-  messagingSenderId: "651326853068",
-  appId: "1:651326853068:web:3bd3817aa24d23241c1769",
-  measurementId: "G-EBC6YEC73P"
-};
